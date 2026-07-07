24c0163837bb4e0ae2ef0405e8211b3dd805ebf7
banuri <banuri.20232356@iit.ac.lk>
2026-07-07 23:33:07 +0530

    complete the frontend

--- src/redux/slices/clientsSlice.js
+++ src/redux/slices/clientsSlice.js
@@ -36,9 +36,53 @@
 //   };
 // };
 
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// // import { fetchClients, fetchClientById } from "../../services/clientsService";
+// import { fetchClients, fetchClientById } from "../../Services/clientsService";
+
+// export const loadClients = createAsyncThunk("clients/loadClients", async () => {
+//   return await fetchClients();
+// });
+
+// export const loadClientDetails = createAsyncThunk(
+//   "clients/loadClientDetails",
+//   async (clientId) => {
+//     return await fetchClientById(clientId);
+//   }
+// );
+
+// const clientsSlice = createSlice({
+//   name: "clients",
+//   initialState: {
+//     list: [], // [{ id, name }]
+//     detailsById: {}, // cache of full address details keyed by id
+//     status: "idle",
+//     error: null,
+//   },
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(loadClients.pending, (state) => {
+//         state.status = "loading";
+//       })
+//       .addCase(loadClients.fulfilled, (state, action) => {
+//         state.status = "succeeded";
+//         state.list = action.payload;
+//       })
+//       .addCase(loadClients.rejected, (state, action) => {
+//         state.status = "failed";
+//         state.error = action.error.message;
+//       })
+//       .addCase(loadClientDetails.fulfilled, (state, action) => {
+//         state.detailsById[action.payload.id] = action.payload;
+//       });
+//   },
+// });
+
+// export default clientsSlice.reducer;
+
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-// import { fetchClients, fetchClientById } from "../../services/clientsService";
-import { fetchClients, fetchClientById } from "../../Services/clientsService";
+import { fetchClients, fetchClientById } from "../../services/clientsService";
 
 export const loadClients = createAsyncThunk("clients/loadClients", async () => {
   return await fetchClients();
